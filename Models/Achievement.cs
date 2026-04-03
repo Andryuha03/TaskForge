5d5814bb42fc21be7e2ddb23f206661362d9ac90
АндрюхаLabtop <andryusha2315@bk.ru>
2026-04-03 09:49:27 +0800

    Добавлена папка Models со всеми вытикающими классами внутри

---
+++ Models/Achievement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskForge.Models
+{
+    class Achievement
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+        public string Criteria { get; set; }
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskForge.Models
+{
+    class Project
+    {
+        public int Id { get; set; }
+        public int User_id { get; set; }
+        public string Name { get; set; }
+        public string Info { get; set; }
+        public DateTime Deadline { get; set; }
+        public string Status { get; set; }
+        public string Color_mark { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Update_at { get; set; }
+    }
+}
+
